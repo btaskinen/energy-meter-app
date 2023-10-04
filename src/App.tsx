968086ebf80ba1bda0d435ef1fbb3bd1f3a29908
Barbara <barbara.taskinen@gmail.com>
2023-10-04 12:32:09 +0300

    Set up Current Reading view
    - for now, hard coded, made up data is used

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,26 @@ import Settings from './components/Settings';
 import CurrentReading from './components/CurrentReading';
 import './App.css';
 
+export type Data = {
+  flowRate: number;
+  energyFlowRate: number;
+  velocity: number;
+  fluidSoundSpeed: number;
+  temperatureInlet: number;
+  temperatureOutlet: number;
+  date: string;
+};
+
 const App = () => {
+  const currentData: Data = {
+    flowRate: 0.75,
+    energyFlowRate: 18.5,
+    velocity: 0.63,
+    fluidSoundSpeed: 1657.1,
+    temperatureInlet: 7.1,
+    temperatureOutlet: 10.8,
+    date: new Date().toString(),
+  };
   const navigate = useNavigate();
 
   const logoutHandler = () => {
@@ -27,7 +46,10 @@ const App = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/flow-history" element={<FlowHistory />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="/current-reading" element={<CurrentReading />} />
+        <Route
+          path="/current-reading"
+          element={<CurrentReading data={currentData} />}
+        />
         <Route path="/" element={<Navigate replace to="/current-reading" />} />
       </Routes>
     </>
--- src/components/CurrentReading.tsx
+++ src/components/CurrentReading.tsx
@@ -1,5 +1,28 @@
-const CurrentReading = () => {
-  return <div>Current Reading</div>;
+import { Data } from '../App';
+
+type Props = {
+  data: Data;
+};
+
+const CurrentReading = ({ data }: Props) => {
+  return (
+    <div>
+      <div>Flow Rate</div>
+      <div>
+        {data.flowRate} m<sup>3</sup>/h
+      </div>
+      <div>Energy Flow Rate</div>
+      <div>{data.energyFlowRate} GJ/h</div>
+      <div>Velocity</div>
+      <div>{data.velocity} m/s</div>
+      <div>Fluid Sound Speed</div>
+      <div>{data.fluidSoundSpeed} m/s</div>
+      <div>Inlet Temperature</div>
+      <div>{data.temperatureInlet} °C</div>
+      <div>Outlet Temperature</div>
+      <div>{data.temperatureOutlet} °C</div>
+    </div>
+  );
 };
 
 export default CurrentReading;
