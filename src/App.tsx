dff43ae484b231621a540559446dc1c1b801c996
Barbara <barbara.taskinen@gmail.com>
2023-10-04 13:20:29 +0300

    Set up basic login functionality
    - created loginform
    - only logged in users can access the content of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import { Routes, Route, Link, Navigate, useNavigate } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import FlowHistory from './components/FlowHistory';
@@ -15,7 +16,15 @@ export type Data = {
   date: string;
 };
 
+type User = {
+  username: string;
+};
+
 const App = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [user, setUser] = useState<User | null>(null);
+
   const currentData: Data = {
     flowRate: 0.75,
     energyFlowRate: 18.5,
@@ -27,13 +36,28 @@ const App = () => {
   };
   const navigate = useNavigate();
 
+  const loginHandler = (event: FormEvent) => {
+    event.preventDefault();
+    const loggedInUser: User = {
+      username,
+    };
+    setUser(loggedInUser);
+    console.log(user);
+    console.log(username, password);
+    setUsername('');
+    setPassword('');
+    navigate('/');
+  };
+
   const logoutHandler = () => {
+    setUser(null);
     navigate('/login');
   };
 
   return (
     <>
       <h1>Cooling Liquid Flow Rate</h1>
+      {user && <p>{user.username} is logged in</p>}
       <nav className="App_navbar">
         <Link to="/">Current Reading</Link>
         <Link to="/flow-history">Flow History</Link>
@@ -43,14 +67,46 @@ const App = () => {
         </button>
       </nav>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/flow-history" element={<FlowHistory />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route
+          path="/login"
+          element={
+            <LoginPage
+              username={username}
+              password={password}
+              setPassword={setPassword}
+              setUsername={setUsername}
+              handleLogin={loginHandler}
+            />
+          }
+        />
+        <Route
+          path="/flow-history"
+          element={user ? <FlowHistory /> : <Navigate replace to="/login" />}
+        />
+        <Route
+          path="/settings"
+          element={user ? <Settings /> : <Navigate replace to="/login" />}
+        />
         <Route
           path="/current-reading"
-          element={<CurrentReading data={currentData} />}
+          element={
+            user ? (
+              <CurrentReading data={currentData} />
+            ) : (
+              <Navigate replace to="/login" />
+            )
+          }
         />
-        <Route path="/" element={<Navigate replace to="/current-reading" />} />
+        <Route
+          path="/"
+          element={
+            user ? (
+              <Navigate replace to="/current-reading" />
+            ) : (
+              <Navigate replace to="/login" />
+            )
+          }
+        />
       </Routes>
     </>
   );
--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,5 +1,45 @@
-const LoginPage = () => {
-  return <div>Login Page</div>;
+import { FormEvent } from 'react';
+
+type Props = {
+  username: string;
+  setUsername: (value: string) => void;
+  password: string;
+  setPassword: (value: string) => void;
+  handleLogin: (event: FormEvent) => void;
+};
+
+const LoginPage = ({
+  username,
+  setUsername,
+  password,
+  setPassword,
+  handleLogin,
+}: Props) => {
+  return (
+    <form className="LoginPage_form" onSubmit={handleLogin}>
+      <div className="LoginPage_inputField">
+        Username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div className="LoginPage_inputField">
+        Password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button className="LoginPage_loginButton" type="submit">
+        Login
+      </button>
+    </form>
+  );
 };
 
 export default LoginPage;
