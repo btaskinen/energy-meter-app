ad00ad9de7f185d6f474eee48a8b4317d3eb07bf
Barbara <barbara.taskinen@gmail.com>
2023-10-05 10:17:54 +0300

    move app into its own module

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const loginRouter = require('./controllers/login');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/login/', loginRouter);
+
+app.get('/', (request, response) => {
+  response.send('<h1>The Server is running!<h1>');
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  next(error);
+};
+
+app.use(errorHandler);
+
+module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,4 @@
-require('dotenv').config();
-const express = require('express');
-const loginRouter = require('./controllers/login');
-
-const app = express();
-
-app.use(express.json());
-
-app.use('/api/login/', loginRouter);
-
-app.get('/', (request, response) => {
-  response.send('<h1>The Server is running!<h1>');
-});
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
-
-app.use(unknownEndpoint);
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  next(error);
-};
-
-app.use(errorHandler);
+const app = require('./app');
 
 const PORT = 3001;
 app.listen(PORT, () => {
