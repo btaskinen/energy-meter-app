39c4f982dce3a9940724f01e5f4cefa80140c87d
Barbara <barbara.taskinen@gmail.com>
2023-10-06 09:30:58 +0300

    create tests for getting all data

--- backend/tests/flow-data.test.js
+++ backend/tests/flow-data.test.js
@@ -5,7 +5,7 @@ const { describe } = require('node:test');
 const api = supertest(app);
 
 describe('testing fetching data from server', () => {
-  test('data is returned as JSON', async () => {
+  test('latest data is returned as JSON', async () => {
     const loginData = {
       username: 'JDoe',
       password: 'secret',
@@ -21,8 +21,30 @@ describe('testing fetching data from server', () => {
       .expect('Content-Type', /application\/json/);
   });
 
-  test('fetching data without authentication', async () => {
+  test('fetching latest data without authentication', async () => {
     const response = await api.get('/api/flow-data/latest-data').expect(400);
+    const returnedMessage = JSON.parse(response.text);
+    expect(returnedMessage.error).toBe('jwt must be provided');
+  });
+
+  test('all data is returned as JSON', async () => {
+    const loginData = {
+      username: 'JDoe',
+      password: 'secret',
+    };
+    const response = await api.post('/api/login').send(loginData);
+
+    const token = response.body.token;
+
+    await api
+      .get('/api/flow-data')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('fetching latest data without authentication', async () => {
+    const response = await api.get('/api/flow-data').expect(400);
     const returnedMessage = JSON.parse(response.text);
     expect(returnedMessage.error).toBe('jwt must be provided');
   });
